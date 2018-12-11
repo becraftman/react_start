2d925bb0d5dcd056c19d7c679f21b65ea7c8c7e0
漏新红 <lou.xinhong@darcytech.com>
2018-12-11 10:47:47 +0800

    提交初始化代码

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+export class HelloWorld extends  Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (<div>
+        Hello World!
+    </div>);
+
+  }
+}
